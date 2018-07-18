d7605a33de7ac48de8bf74c180634f6bac8efb09
samcompu <a1592710567@126.com>
2018-07-18 18:04:28 +0800

    update v1.0.1

--- index.php
+++ index.php
@@ -4,22 +4,30 @@
   <meta charset="UTF-8">
   <title>简单翻译</title>
   <link rel="stylesheet" href="./transPage.css">
+  <script src="https://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+  <script src="./transPage.js"></script>
 </head>
-<body>
+<body style="<?php echo("background-image: url('./backImg/".rand(0, 1).".jpg')"); ?>">
   <?php include "./trans.php" ?>
   <div class="wallDiv">
     <div class="fontDiv">
       <div class="titleDiv">简单翻译</div>
+
       <form method="POST" action="./index.php" id="transForm"></form>
+
       <div class="textDiv">
-        <textarea form="transForm" name="transToText" placeholder="请在此输入需要翻译的文本……" wrap="virtual"><?php if ($tBAns) echo($tBAns); ?></textarea>
-        <textarea form="transForm" name="transBackText" placeholder="翻译的结果在这里……" wrap="virtual"><?php if ($tTAns) echo($tTAns); ?></textarea><br/>
+        <textarea form="transForm" name="transToText" placeholder="请在此输入需要翻译的文本……" wrap="virtual"><?php if ($way == "to") echo($transText); else echo($transToAns); ?></textarea>
+        <textarea form="transForm" name="transBackText" placeholder="翻译的结果在这里……" wrap="virtual"><?php if ($way == "back") echo($transText); else echo($transToAns); ?></textarea><br/>
       </div>
-      <button type="submit" form="transForm" name="transTo">TransTo</button>
-      <button type="submit" form="transForm" name="transBack">TransBack</button></br>
+      <div class="buttonDiv">
+        <div class="clickDiv" id="transToButton" onclick="transTo()">TransTo</div>
+        <div class="clickDiv" id="transBackButton" onclick="transBack()">TransBack</div>
+      </div>
+
       <div class="footerDiv">
-        <p>原文有种更好的说法：<?php if ($bTTAns) echo($bTTAns); elseif ($bTBAns) echo($bTBAns);?></p>
+        <p>原文有种更好的说法：<?php echo($transBackAns);?></p>
       </div>
+
     </div>
   </div>
 </body>
--- trans.php
+++ trans.php
@@ -1,34 +1,29 @@
 <?php
 include './baidu_transapi.php';
 
-if (isset($_COOKIE['transFrom'])) $from = $_COOKIE['transFrom'];
-else $from = "auto";
-if (isset($_COOKIE['transTo'])) $to = $_COOKIE['transTo'];
-else $to = "zh";
+$way = $_COOKIE['transWay'];
 
-if (isset($_POST['transTo'])) {
+if ($way == "to") {
   $from = "auto";
-  $to = "zh";
   setcookie('transFrom', "auto");
-  setcookie('transTo', "zh");
-  $transToText = $_POST['transToText'];
-
-  /** 过滤输入文本中的换行，百度翻译API不支持翻译换行*/
-  $transToText = str_replace(PHP_EOL, '', $transToText);
-
-  $transToAns = translate($transToText, $from, $to);
-  $tTAns = $transToAns["trans_result"][0]["dst"];
-  $from = $transToAns["from"];
-  setcookie('transFrom', $from);
-  $betterTransBack = translate($tTAns, $to, $from);
-  $bTBAns = $betterTransBack["trans_result"][0]["dst"];
+  $to = "zh";
+  $transText = $_POST['transToText'];
+} else {
+  $from = "zh";
+  if (isset($_COOKIE['transFrom'])) $to = $_COOKIE['transFrom'];
+  else $to = "en";
+  $transText = $_POST['transBackText'];
 }
 
-if (isset($_POST['transBack'])) {
-  $transBackText = $_POST['transBackText'];
-  $transBackAns = translate($transBackText, $to, $from);
-  $tBAns = $transBackAns["trans_result"][0]["dst"];
-  $betterTransTo = translate($tBAns, $from, $to);
-  $bTTAns = $betterTransTo["trans_result"][0]["dst"];
-}
+/** 过滤输入文本中的换行，百度翻译API不支持翻译换行*/
+$transText = str_replace(PHP_EOL, '', $transText);
+
+$transTo = translate($transText, $from, $to);
+$from = $transTo["from"];
+setcookie('transFrom', $from);
+$transToAns = $transTo["trans_result"][0]["dst"];
+$transBack = translate($transToAns, $to, $from);
+$transBackAns = $transBack["trans_result"][0]["dst"];
+
+if ($transText == "简单翻译") $transBackAns = "由 Samcompu 制作.";
 ?>
